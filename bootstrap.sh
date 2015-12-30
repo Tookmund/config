92887e4112c9f7417d2808907c1371ca2248ba4b
Jacob Adams <tookmund@gmail.com>
2015-12-30 13:30:31 -0500

    bootstrap.sh: Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Bootstrap and remove my config files
+# Assumes this is cloned to $HOME/config
+# ./bootstrap.sh ln to hardlink all files
+# ./bootstrap.sh unlink to unlink all files
+# ./bootstrap.sh cp to copy all files
+# etc.
+
+for i in profile devscripts xinitrc quiltrc-dpkg nanorc dput.cf lintianrc
+do
+	$1 $i $HOME/.$i
+done

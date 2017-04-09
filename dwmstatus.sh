8dd53cfbe067884661429f39206a78a6e0ff9c38
Jacob Adams <tookmund@gmail.com>
2017-04-09 08:52:49 -0400

    dwmstatus: Fix up battery and volume prints

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -4,14 +4,14 @@
 
 while true
 do
-	VOL=$(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')
+	VOL="Vol: $(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')"
 	LOCALTIME=$(date +%Y-%m-%d\ %H:%M)
 	IP=$(for i in `ip r`; do echo $i; done | grep -A 1 src | tail -n1) # can get confused if you use vmware
 	TEMP="$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))C"
 
 	if acpi -a | grep off-line > /dev/null
 	then
-		BAT="Bat. $(acpi -b | awk '{ print $4 " " $5 }' | tr -d ',')"
+		BAT="Bat: $(acpi -b | awk '{ print $4 " " $5 }' | tr -d ',')"
 		xsetroot -name "$IP $BAT $VOL $TEMP $LOCALTIME"
 	else
 		xsetroot -name "$IP $VOL $TEMP $LOCALTIME" 

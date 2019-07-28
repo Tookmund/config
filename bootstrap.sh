b6779072f811014a635ec4e94603bc334cef39ac
Jacob Adams <jacob@tookmund.com>
2019-07-28 23:04:59 +0100

    bootstrap: Don't assume directories exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,10 +15,13 @@ done
 
 # i3wm
 $MV "$PWD/i3.xsession" "$HOME/.xsession"
+mkdir -p "$HOME/.config/i3/"
 $MV "$PWD/i3config" "$HOME/.config/i3/config"
+mkdir -p "$HOME/.config/i3status/"
 $MV "$PWD/i3status.conf" "$HOME/.config/i3status/config"
 
 # GPG Configuration File
+mkdir -p "$HOME/.gnupg/"
 $MV "$PWD/gpg.conf" "$HOME/.gnupg/gpg.conf"
 $MV "$PWD/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
 

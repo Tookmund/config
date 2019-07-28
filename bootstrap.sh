4839709b2147b8e67dbacfbd87b7edefc7798ad7
Jacob Adams <jacob@tookmund.com>
2019-07-28 22:39:19 +0100

    bootstrap: Enable symbolic linking by using full paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,17 @@
 # ./bootstrap.sh cp to copy all files
 # etc.
 
-for i in profile devscripts xinitrc quiltrc-dpkg nanorc dput.cf lintianrc
+
+for i in profile devscripts quiltrc-dpkg nanorc dput.cf lintianrc
 do
-	$1 $i $HOME/.$i
+	$1 "$PWD/$i" "$HOME/.$i"
 done
 
+# i3wm
+$1 "$PWD/i3.xsession" "$HOME/.xsession"
+$1 "$PWD/i3config" "$HOME/.config/i3/config"
+$1 "$PWD/i3status.conf" "$HOME/.config/i3status/config"
+
 # GPG Configuration File
-$1 gpg.conf $HOME/.gnupg/gpg.conf
+$1 "$PWD/gpg.conf" "$HOME/.gnupg/gpg.conf"
+$1 "$PWD/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"

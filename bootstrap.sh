e5b68b7f675da670a4feb859b4a739d494d865d3
Jacob Adams <jacob@tookmund.com>
2019-07-28 22:50:47 +0100

    bootstrap: Default to symbolic links

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,20 +6,21 @@
 # ./bootstrap.sh cp to copy all files
 # etc.
 
+MV=${1:-ln -s}
 
 for i in profile devscripts quiltrc-dpkg nanorc dput.cf lintianrc
 do
-	$1 "$PWD/$i" "$HOME/.$i"
+	$MV "$PWD/$i" "$HOME/.$i"
 done
 
 # i3wm
-$1 "$PWD/i3.xsession" "$HOME/.xsession"
-$1 "$PWD/i3config" "$HOME/.config/i3/config"
-$1 "$PWD/i3status.conf" "$HOME/.config/i3status/config"
+$MV "$PWD/i3.xsession" "$HOME/.xsession"
+$MV "$PWD/i3config" "$HOME/.config/i3/config"
+$MV "$PWD/i3status.conf" "$HOME/.config/i3status/config"
 
 # GPG Configuration File
-$1 "$PWD/gpg.conf" "$HOME/.gnupg/gpg.conf"
-$1 "$PWD/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
+$MV "$PWD/gpg.conf" "$HOME/.gnupg/gpg.conf"
+$MV "$PWD/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
 
 #Neovim
-$1 "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
+$MV "$PWD/init.vim" "$HOME/.config/nvim/init.vim"

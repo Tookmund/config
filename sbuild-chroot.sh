fd7a52ce767bc958cf6587243f2cf43d4eb5f8b1
Jacob Adams <jacob@tookmund.com>
2025-08-24 11:24:30 -0400

    replace sudo with doas

--- sbuild-chroot.sh
+++ sbuild-chroot.sh
@@ -1,1 +1,1 @@
-sudo sbuild-createchroot --include=eatmydata,ccache,gnupg $1 /srv/chroot/$1-amd64-sbuild http://deb.debian.org/debian
+doas sbuild-createchroot --include=eatmydata,ccache,gnupg $1 /srv/chroot/$1-amd64-sbuild http://deb.debian.org/debian

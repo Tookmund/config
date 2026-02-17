c0c3b20d8610b7bfb286c6cb3eac327da60a2892
Jacob Adams <jacob@tookmund.com>
2026-02-16 21:30:28 -0500

    init.lua: reformat with lua-language-server

--- init.lua
+++ init.lua
@@ -29,15 +29,15 @@ set sm
 vim.lsp.config['gopls'] = {
 	cmd = { 'gopls' },
 	filetypes = { 'go' },
-	  settings = {
-	    gopls = {
-	      analyses = {
-			unusedparams = true,
-	      },
-	      staticcheck = true,
-	      gofumpt = true,
-	    },
-	  },
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
 }
 
 vim.lsp.enable('gopls')
@@ -45,27 +45,27 @@ vim.lsp.enable('gopls')
 vim.lsp.config['rust_analyzer'] = {
 	cmd = { 'rust-analyzer' },
 	filetypes = { 'rust' },
-	  settings = {
-	    rust_analyzer = {
-            imports = {
+	settings = {
+		rust_analyzer = {
+			imports = {
 				granularity = {
 					group = "module",
-                },
-                prefix = "self",
-            },
-            cargo = {
-                buildScripts = {
-                    enable = true,
-                },
-            },
-            procMacro = {
-                enable = true
-            },
+				},
+				prefix = "self",
+			},
+			cargo = {
+				buildScripts = {
+					enable = true,
+				},
+			},
+			procMacro = {
+				enable = true
+			},
 		},
-	  },
+	},
 }
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	pattern = "*.rs",
 	callback = function()
 		vim.lsp.buf.format({ async = false })
@@ -75,28 +75,28 @@ vim.api.nvim_create_autocmd({"BufWritePre"}, {
 vim.lsp.enable('rust_analyzer')
 
 vim.lsp.config['lua_ls'] = {
-  -- Command and arguments to start the server.
-  cmd = { 'lua-language-server' },
-  -- Filetypes to automatically attach to.
-  filetypes = { 'lua' },
-  -- Sets the "workspace" to the directory where any of these files is found.
-  -- Files that share a root directory will reuse the LSP server connection.
-  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
-  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-  -- Specific settings to send to the server. The schema is server-defined.
-  -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'Lua 5.4',
-      }
-    }
-  }
+	-- Command and arguments to start the server.
+	cmd = { 'lua-language-server' },
+	-- Filetypes to automatically attach to.
+	filetypes = { 'lua' },
+	-- Sets the "workspace" to the directory where any of these files is found.
+	-- Files that share a root directory will reuse the LSP server connection.
+	-- Nested lists indicate equal priority, see |vim.lsp.Config|.
+	root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+	-- Specific settings to send to the server. The schema is server-defined.
+	-- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'Lua 5.4',
+			}
+		}
+	}
 }
 
 vim.lsp.enable('lua_ls')
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	pattern = "*.lua",
 	callback = function()
 		vim.lsp.buf.format({ async = false })
@@ -104,31 +104,30 @@ vim.api.nvim_create_autocmd({"BufWritePre"}, {
 })
 
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  pattern = "*.go",
-  callback = function()
-    local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
-    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-    -- machine and codebase, you may want longer. Add an additional
-    -- argument after params if you find that you have to write the file
-    -- twice for changes to be saved.
-    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-    vim.lsp.buf.format({async = false})
-  end
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = "*.go",
+	callback = function()
+		local params = vim.lsp.util.make_range_params()
+		params.context = { only = { "source.organizeImports" } }
+		-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+		-- machine and codebase, you may want longer. Add an additional
+		-- argument after params if you find that you have to write the file
+		-- twice for changes to be saved.
+		-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+		for cid, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+					vim.lsp.util.apply_workspace_edit(r.edit, enc)
+				end
+			end
+		end
+		vim.lsp.buf.format({ async = false })
+	end
 })
 
 vim.diagnostic.enable = true
 vim.diagnostic.config({
 	virtual_lines = true,
 })
-
